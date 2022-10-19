4924651c996fca2ea00516405cfb4d8239aa4c96
tokorom <tokorom@gmail.com>
2022-10-19 16:53:32 +0900

    First commit

---
+++ AudioEngineSample/AudioEngineSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioEngineSampleApp.swift
+//  AudioEngineSample
+//
+//  Created by Yuta Tokoro on 2022/10/19.
+//
+
+import SwiftUI
+
+@main
+struct AudioEngineSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
